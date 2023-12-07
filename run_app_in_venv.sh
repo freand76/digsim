3000700cea3ea56a85416c401cae2d791f28ee3d
Fredrik Andersson <freand@gmail.com>
2023-12-07 20:42:13 +0100

    Add script to start digsim application in venv

---
+++ run_app_in_venv.sh
@@ -0,0 +1,17 @@
+SCRIPT_DIR=$(dirname $0)
+VENV_DIR=$SCRIPT_DIR/digsim_venv
+
+if [ ! -f $VENV_DIR/digsim_installed ]; then
+    echo "Creating virtual environment"
+    python3 -m venv $VENV_DIR
+    source $VENV_DIR/bin/activate
+    pip3 install pip --upgrade
+    pip3 install .
+    touch $VENV_DIR/digsim_installed
+else
+    echo "Virtual environment already created"
+    source $VENV_DIR/bin/activate
+fi
+
+echo "Starting DigSim application"
+python3 -m digsim.app $@
