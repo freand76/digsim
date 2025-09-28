67cca70a3cb5f5cd2af237868878cfa22a1fe1ee
Fredrik Andersson <freand@gmail.com>
2025-09-28 19:08:03 +0200

    Add some more inline documentation

--- examples/yosys_6502/example_yosys_6502.py
+++ examples/yosys_6502/example_yosys_6502.py
@@ -25,43 +25,55 @@ from digsim.circuit.components import (
 )
 
 
-# Get the relative path to example folder
+# Get the relative path to the example folder
 example_path = os.path.relpath(os.path.dirname(os.path.abspath(__file__)), os.getcwd())
 
+# Create a new circuit with VCD output
 test_circuit = Circuit(vcd="6502.vcd")
+
+# Instantiate components
 rst = PushButton(test_circuit, "RST")
 clk = Clock(test_circuit, frequency=1000000, name="CLK")
 irq = StaticValue(test_circuit, "IRQ", value=0)
 rdy = StaticValue(test_circuit, "RDY", value=1)
 
+# Instantiate the 6502 Yosys component, memory, and stdout output
 yosys_6502 = YosysComponent(test_circuit, path=f"{example_path}/6502.json")
 mem = Mem64kByte(test_circuit, rom_filename=f"{example_path}/code.bin", rom_address=0xF800)
 output = MemStdOut(test_circuit, address=0x8000)
 
+# --- Wire up the components ---
+# Clock and memory
 clk.wire = mem.clk
 yosys_6502.AB.wire = mem.Address
 yosys_6502.DO.wire = mem.DataIn
 yosys_6502.WE.wire = mem.WE
 mem.DataOut.wire = yosys_6502.DI
 
+# Clock and stdout output
 clk.wire = output.clk
 yosys_6502.AB.wire = output.Address
 yosys_6502.DO.wire = output.DataIn
 yosys_6502.WE.wire = output.WE
 
+# Control signals for 6502
 rst.O.wire = yosys_6502.reset
 clk.O.wire = yosys_6502.clk
 irq.O.wire = yosys_6502.IRQ
 irq.O.wire = yosys_6502.NMI
 rdy.O.wire = yosys_6502.RDY
 
+# Initialize the circuit
 test_circuit.init()
 print(yosys_6502)
 
+# --- Simulation sequence ---
+# Apply and release reset
 rst.push()
 test_circuit.run(us=1)
 rst.release()
 test_circuit.run(us=1)
 
+# Run simulation for a number of clock cycles
 for _ in range(300):
     test_circuit.run(us=1)
